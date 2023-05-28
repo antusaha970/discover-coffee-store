45f9735d3eb318d69fb578ad9417ceabf42457c6
Antu Saha <antusaha970@gmail.com>
2023-05-28 21:56:00 +0600

    added card and it's style

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
@@ -5,12 +6,13 @@ import React from "react";
 const CoffeeStore = () => {
   const router = useRouter();
   const { id } = router.query;
-  console.log(router);
   return (
     <div>
+      <Head>
+        <title>{id}</title>
+      </Head>
       <h1>CoffeeStore {id}</h1>
       <Link href="/">Back to home</Link>
-      <Link href="/coffee-store/dynamic">Back to home</Link>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/Banner/Banner";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Image from "next/image";
+import Card from "../components/Card/Card";
 
 export default function Home() {
   const handleBannerButtonClick = () => {
@@ -22,7 +21,34 @@ export default function Home() {
           buttonText="view nearby coffee shops"
           handleClick={handleBannerButtonClick}
         />
-        <Link href="/courses/nextjs">got to course</Link>
+        <div className={styles.heroImage}>
+          <Image
+            src="/static/hero-image.png"
+            width={700}
+            height={400}
+            alt="hero image"
+          />
+        </div>
+        <div className={styles.cardLayout}>
+          <Card
+            href="/coffee-store/Raju Coffee Store"
+            imgUrl="/static/hero-image.png"
+            name="Raju Coffee Store"
+            className={styles.card}
+          />
+          <Card
+            href="/coffee-store/Raju Coffee Store"
+            imgUrl="/static/hero-image.png"
+            name="Raju Coffee Store"
+            className={styles.card}
+          />
+          <Card
+            href="/coffee-store/Raju Coffee Store"
+            imgUrl="/static/hero-image.png"
+            name="Raju Coffee Store"
+            className={styles.card}
+          />
+        </div>
       </main>
     </div>
   );
